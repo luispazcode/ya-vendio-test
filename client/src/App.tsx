67ceabe946c86786d41f7f24577d14da3c895224
Luis Paz Cordova <lpaz0073@gmail.com>
2024-11-20 16:25:57 -0500

    feat: init client project with vite

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import ChatApp from "./components/ChatApp";
+
+function App() {
+	return (
+		<main className='w-full h-screen max-w-2xl flex flex-col justify-center m-auto z-50 font-geist p-4'>
+			<ChatApp />
+		</main>
+	);
+}
+
+export default App;
